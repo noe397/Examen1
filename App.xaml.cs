c72010eaf046694d607c46f3ef81630931bc07e9
noe397 <117100843+noe397@users.noreply.github.com>
2022-10-31 09:31:27 -0600

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen
+{
+    public partial class App : Application
+    {
+        static Controllers.DBEmple dBEmple;
+
+
+        public static Controllers.DBEmple dbemple
+        {
+            get
+            {
+                if (dBEmple == null)
+                {
+                    string DBName = "emple.db3";
+                    string PathDB = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DBName);
+                   dBEmple = new Controllers.DBEmple(PathDB);
+                }
+
+                return dBEmple;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.PagePrinicpal());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
